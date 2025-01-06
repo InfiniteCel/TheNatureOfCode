2541cc3f27afc232fd8da4b23564779c77e700ea
David Fuller <dragonslayerdavide@gmail.com>
2025-01-05 22:40:55 -0700

    pg 6 example 0.1: Trying out initial Walker class implementation

--- p5test/sketch.js
+++ p5test/sketch.js
@@ -1,8 +1,63 @@
+let walkers = [];
 function setup() {
   createCanvas(400, 400);
+  background(220);
+  
+  //TODO: Can I source this from a separate Walker.js file somehow?
+  class Walker {
+    constructor() {
+      this.x = width / 2;
+      this.y = height / 2;
+    }
+
+    show() {
+      stroke( 0 );
+      strokeWeight( 2 );
+      point( this.x, this.y );
+      // set( this.x, this.y,  0);
+      // updatePixels();
+    }
+    
+    step() {
+      const choice = Math.floor( random( 4 ) );
+      switch ( choice ) {
+        case 0:
+          //Up
+          this.y--;
+          break;
+        case 1:
+          //Down
+          this.y++;
+          break;
+        case 2:
+          //Left
+          this.x--;
+          break;
+        case 3:
+          //Right
+          this.x++;
+          break    
+        default:
+          console.error("Error in Walker step");
+          break;
+      }
+    }
+  }
+
+  walkers = [ 
+    new Walker(), 
+    new Walker(), 
+    new Walker(), 
+    new Walker(), 
+    new Walker()
+  ];
 }
 
 function draw() {
-  background(220);
-  circle(100,100,100).fill(204, 102, 0);
+  walkers.forEach( function( walker ) {
+    walker.step();
+    walker.show();
+  });
 }
+
+
